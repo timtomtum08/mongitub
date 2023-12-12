79dd1d6162067bbc3b1e25ceccca6f914c4d14b5
argonaultes <gael@argonaultes.fr>
2023-12-12 11:38:32 +0100

    listing des qcms sous forme d'objets

--- models/inmemory.js
+++ models/inmemory.js
@@ -1,3 +1,13 @@
-const qcms = ['Javascript', 'Angular', 'Express'];
+// importer la classe Qcm
+const Qcm = require('./qcm');
+
+// remplacer les simples chaines de caracteres par des instances de la classe Qcm
+const qcms = [
+    new Qcm({id: 0, name: 'Introduction Vanilla JS', nbpoints: 20, subject: 'Javascript'}),
+    new Qcm({id: 1, name: 'Framework Frontend', nbpoints: 20, subject: 'Angular'}),
+    new Qcm({id: 2, name: 'Framework Backend', nbpoints: 10, subject: 'Express'}),
+        ];
+
+        //FIXME: fonction qui ajoute un element a la liste et incremente l'id
 
 module.exports = qcms;
---
+++ models/qcm.js
@@ -0,0 +1,50 @@
+// id, name, theme, subject, author, nbpoints
+
+class Qcm {
+
+    #id;
+    #name;
+    #theme;
+    #subject;
+    #author;
+    #nbpoints;
+
+    constructor(qcmToCreate) {
+        this.#id = qcmToCreate.id;
+        this.#name = qcmToCreate.name;
+        this.#theme = qcmToCreate.theme;
+        this.#subject = qcmToCreate.subject;
+        this.#author = qcmToCreate.author;
+        this.#nbpoints = qcmToCreate.nbpoints;
+    }
+
+    get id() {
+        return this.#id;
+    }
+
+    get name() {
+        return this.#name;
+    }
+
+    set name(value) {
+        this.#name = value;
+    }
+
+    get theme() {
+        return this.#theme;
+    }
+
+    get subject() {
+        return this.#subject;
+    }
+
+    get author() {
+        return this.#author;
+    }
+
+    get nbpoints() {
+        return this.#nbpoints;
+    }
+}
+
+module.exports = Qcm;
